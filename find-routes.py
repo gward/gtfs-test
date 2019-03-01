f18761402c313ce93ac71c04c187420ad171257d
Greg Ward <greg@gerg.ca>
2019-03-01 16:36:33 -0500

    add docstrings and comments

--- find-routes.py
+++ find-routes.py
@@ -21,6 +21,7 @@ def main():
 
 
 def find_stop_ids(stops_fh, want_name):
+    """parse stops.txt, yield sequence of stop IDs based on want_name"""
     (reader, indexes) = open_csv(stops_fh, 'stop_id', 'stop_name')
     for line in reader:
         if line[indexes.stop_name] == want_name:
@@ -28,6 +29,7 @@ def find_stop_ids(stops_fh, want_name):
 
 
 def find_trip_ids(stoptimes_fh, stop_ids):
+    """parse stop_times.txt, yield sequence of trip IDs based on stop_ids"""
     (reader, indexes) = open_csv(stoptimes_fh, 'stop_id', 'trip_id')
     for line in reader:
         if line[indexes.stop_id] in stop_ids:
@@ -35,7 +37,11 @@ def find_trip_ids(stoptimes_fh, stop_ids):
 
 
 def find_routes(trips_fh, trip_ids):
+    """parse trips.txt, yield sequence of route IDs based on trip_ids"""
     (reader, indexes) = open_csv(trips_fh, 'trip_id', 'route_id')
+
+    # keep track of unique route IDs in order to preserve order, and so the
+    # caller can print route IDs as they arrive
     seen = set()
     for line in reader:
         if line[indexes.trip_id] in trip_ids:
@@ -51,6 +57,13 @@ class Indexes:
 
 
 def open_csv(fh, *want_columns):
+    """wrap csv.reader around fh and look for wanted columns
+
+    Return (reader, indexes) where indexes is an object with one attribute
+    for every column in want_columns.
+
+    Raise ValueError if the file is missing any of your wanted columns.
+    """
     reader = csv.reader(fh)
     headers = next(reader)
     indexes = Indexes()
